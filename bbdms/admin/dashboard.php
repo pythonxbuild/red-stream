6d3b24123d691f7a0a71adda217c1f61b975552d
Krishna Luharuka <rahdhekrishnaa@gmail.com>
2025-01-25 20:32:20 +0545

    <for real i have just changed the admin password to admin123 by editing the  admin table's structure of password to md5>

--- bbdms/admin/dashboard.php
+++ bbdms/admin/dashboard.php
@@ -103,7 +103,7 @@ $results6=$query6->fetchAll(PDO::FETCH_OBJ);
 $query=$query6->rowCount();
 ?>
 													<div class="stat-panel-number h1 "><?php echo htmlentities($query);?></div>
-													<div class="stat-panel-title text-uppercase">Total Quries</div>
+													<div class="stat-panel-title text-uppercase">Total Queries</div>
 												</div>
 											</div>
 											<a href="manage-conactusquery.php" class="block-anchor panel-footer text-center">Full Detail &nbsp; <i class="fa fa-arrow-right"></i></a>
